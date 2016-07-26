a297f777953c9f6a2dbb2b47b8170c975c860447
Alberto Sola <albsolac@gmail.com>
2016-07-26 12:41:11 +0200

    Apache license.

--- jadereader/src/feed/feedmanager.py
+++ jadereader/src/feed/feedmanager.py
@@ -1,3 +1,19 @@
+"""
+Copyright 2016 Alberto Sola
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
 import feedparser
 
 class FeedManager:
@@ -28,7 +44,7 @@ class FeedManager:
     def download_feed(self, url):
         feed_url = url.get_feed()
         feed_list = []
-        #feed_tree = 
+        #feed_tree =
 
         self.feed[url.get_url()] = feed_list
 
